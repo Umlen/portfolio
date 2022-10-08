10452c8fb801523ae3687479ae3495b0cd083b57
Viktor <victorpatrin@gmail.com>
2022-10-08 17:18:34 +0700

    projects & contacts

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ for (let project of projects) {
     projectsContainerEl.innerHTML += `
         <div class="project">
             <h4>${project.name}</h4>
-            <img src="${project.screenshot}" alt="">
-            <a href="${project.github}" class="section-links" >Github</a>
+            <a href="${project.demo}">
+                <img class="project-img" src="${project.screenshot}" alt="">
+            </a>
+            <a href="${project.code}" class="section-links">Code</a>
             <a href="${project.demo}" class="section-links">Demo</a>
         </div>
     `;
